400e34afe6fb0f670d724feffc39f4d32d6636d3
Joe Longstreet <joelongstreet@gmail.com>
2015-01-21 21:31:32 -0600

    write basic program to compile jade files into html

---
+++ builder.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var jade = require('jade');
+var config = null;
+
+
+var buildConfig = function(){
+  config = JSON.parse(fs.readFileSync('./config.json', { encoding : 'utf8' }));
+};
+
+
+var build = function(){
+  config.forEach(function(item){
+    compileJadeFile('src/' + item.file + '.jade');
+  });
+};
+
+
+var serve = function(){
+  require('live-server').start(3000, process.cwd(), true);
+
+  fs.watch('./src', function(e, filename){
+    compileJadeFile('src/' + filename);
+  });
+
+  fs.watch('./config.json', buildConfig);
+};
+
+
+var findDetailsForFile = function(filename){
+  var details;
+  var abbreviated = filename.replace('src/', '').replace('.jade', '');
+
+  config.forEach(function(item){
+    if(item.file == abbreviated)
+      details = item;
+  });
+
+  return details;
+};
+
+
+var compileJadeFile = function(filename){
+  var details = findDetailsForFile(filename);
+
+  var jadeFile = fs.readFileSync(filename,
+    { encoding : 'utf8' }
+  );
+
+  if(details.file){
+    fs.writeFile('./public/' + details.file + '.html',
+      jade.compile(jadeFile, {
+        pretty    : true,
+        filename  : 'uhhhh.html'
+      })(details)
+    );
+  }
+};
+
+
+buildConfig();
+build();
+process.argv.forEach(function(val, i){
+  if(i == 2 && val == 'serve') serve();
+});
